9d4c80274a8e82dbf6f8ee0496514db0c1be69aa
Ameya-bit <ameyapanchal011@gmail.com>
2026-09-23 20:58:35 -0400

    finish functions for determining next states and dfs

--- solution_q1.py
+++ solution_q1.py
@@ -1,6 +1,5 @@
 state = (3, 3, 0, 0, 0)
 MOVES = [(2, 0), (0, 2), (1, 1), (1, 0), (0, 1)]
-stack = []
 
 def next_state(current_state, m, c): 
     new_state = list(current_state)
@@ -19,11 +18,22 @@ def next_states(state):
     successors = []
     for m, c in MOVES:
         s = next_state(state, m, c)
+        if is_valid(s):
+            successors.append(s)
+    return successors
         
 
 def dfs(state, path, visited):
     if state == (0, 0, 3, 3, 1):
         return path
     visited.add(state)
-    
+    for s in next_states(state):
+        if s not in visited:
+            result = dfs(s, path + [s], visited)
+            if result is not None:
+                return result
+    return None
 
+solution = dfs(state, [state], set())
+for step in solution:
+    print(step)
