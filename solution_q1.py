bfa0511ee57ec0b9c1c84d19f2a587554e7e6f54
Ameya-bit <ameyapanchal011@gmail.com>
2026-09-23 20:55:56 -0400

    add function for checking valid state, and scaffolding for determining next states and dfs

--- solution_q1.py
+++ solution_q1.py
@@ -1,5 +1,5 @@
 state = (3, 3, 0, 0, 0)
-boat_is_left = "L" if state[4] == 0 else "R"
+MOVES = [(2, 0), (0, 2), (1, 1), (1, 0), (0, 1)]
 stack = []
 
 def next_state(current_state, m, c): 
@@ -9,6 +9,21 @@ def next_state(current_state, m, c):
 
     return (ml - d*m,  cl - d*c, mr + d*m, cr + d*c, 1 - boat)
 
+def is_valid(state):
+    ml, cl, mr, cr, _ = state
+    if (min(ml, cl, mr, cr) < 0) or (ml > 0 and cl > ml) or (mr > 0 and cr > mr): 
+        return False
+    return True
 
+def next_states(state):
+    successors = []
+    for m, c in MOVES:
+        s = next_state(state, m, c)
+        
+
+def dfs(state, path, visited):
+    if state == (0, 0, 3, 3, 1):
+        return path
+    visited.add(state)
     
 
